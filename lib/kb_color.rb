03756683399e4743142bc36facf105dcafba3ef4
inpwjp <inpw@mua.biglobe.ne.jp>
2015-09-02 17:15:23 +0900

    update

--- lib/kb_color.rb
+++ lib/kb_color.rb
@@ -1,3 +1,7 @@
 Class KbColor
   attr_accessor r,g,b,v;
+
+  def string()
+    r.to_int
+  end
 end
--- lib/kb_colors.rb
+++ lib/kb_colors.rb
@@ -1,15 +1,39 @@
 Class KbColors
+  attr_accessor header
   attr_accessor title
   attr_accessor kb_colors[]
   MaxLength=15
+
   def initializer(opts = [])
     kb_colors = Array.new()
     if opts[:title].presence?
       title = title
     end
+    header = "Copyright RUIFAN"
   end
 
   def add_color(r = 0, g = 0, b = 0, v = 0)
+    if kb_colors.length >= MaxLength
+      return false
+    end
     kb_color = KbColor.new(r,g,b,v)
+    kb_colors.push kb_color
+    return kb_colors.length
+  end
+
+  def set_title(tmp_title = "")
+    title = tmp_title
+  end
+
+  def delete_at(at)
+    if at.nil? or at >= kb_colors.length - 1
+      return false
+    end
+    kb_colors.delete_at at
+    return self
+  end
+
+  def length()
+    return kb_colors.length
   end
 end
